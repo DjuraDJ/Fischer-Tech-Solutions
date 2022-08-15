731bfa9cf5b6719e83a0c467663a26723ea8f9b3
Djura.Djurickovic <Djura.Djurickovic@gmail.com>
2022-08-15 16:08:22 -0400

    Updated website

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDom from "react-dom";
+
 import AppMain from './Components/AppMain';
 import "./Components/Header/Header.css";
 import "./Components/Parallax/Parallax.css";
 import "./Components/Contact/Contact.css";
+import "./Components/Footer/Footer.css";
 //import "bootstrap/dist/css/bootstrap.min.css";
-
 
 
 ReactDom.render( < AppMain / > , document.getElementById("root"));
